c4fb3e0983c94f2cffe0bf37a308a805a5f70cf2
athulbabu123 <149316874+athulbabu123@users.noreply.github.com>
2024-07-11 16:27:51 +0530

    added card layout

--- src/components/CardApi.jsx
+++ src/components/CardApi.jsx
@@ -1,4 +1,5 @@
 import { Button, Card, CardActions, CardContent, CardMedia, Grid, Typography } from '@mui/material'
+import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 
 const CardApi = () => {
@@ -6,11 +7,53 @@ const CardApi = () => {
 
     var[output,setOutput]=useState([]);
 
+    useEffect(() => {
+        axios.get("https://dummyapi.online/api/pokemon")
+        .then((res) => {
+            console.log(res.data);
+            setOutput(res.data);
+        }).
+        catch((err) =>{
+            console.log(err)})
+    },[]);
+
     
   return (
     <div>
         <Typography>hello</Typography>
+        <Grid container space = {2}>
+            
+                {output.map((val,i)=>{
+                    return(
+                        <Grid item xs={12} md={4} >
+                        
+                        <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        sx={{ height: 140 }}
+        image={val.image_url}
+        title={val.pokemon}
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {val.pokemon}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Share</Button>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+    </Grid>
+                    )
+                })}
 
+            
+
+        </Grid>
     </div>
   )
 }
